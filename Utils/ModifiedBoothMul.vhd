19f26a8fdd0fd9d9d24e3c22f4f19023552f937c
Shehab <Shehabalaa97@gmail.com>
2019-03-11 14:32:03 +0200

    Simplyfing working signal logic

--- Utils/ModifiedBoothMul.vhd
+++ Utils/ModifiedBoothMul.vhd
@@ -18,7 +18,8 @@ ARCHITECTURE ModifiedBoothMulArch OF ModifiedBoothMul IS
     SIGNAL pInit,pBs,pMux,pReg : STD_LOGIC_VECTOR(2*n DOWNTO 0);
     SIGNAL mReg : STD_LOGIC_VECTOR(n-1 DOWNTO 0); 
     SIGNAL counter : STD_LOGIC_VECTOR(n/2 DOWNTO 0);
-    SIGNAL counterRst,restartDetection,startAndPause,qBar,tffClk :STD_LOGIC;
+    SIGNAL counterRst,restartDetection,startAndPause,firstStart :STD_LOGIC;
+    
 BEGIN
     -- algo paramters intialaization
     pInit <= c & q &'0';
@@ -27,16 +28,16 @@ BEGIN
     done <= counter(n/2-1);
     startAndPause <= NOT done;
     counterRst <= rst OR restartDetection;
-    tffClk <= restartDetection OR Done;
+    working <= firstStart AND NOT done; 
 
-    -- create and wire circuit main compenet
+    -- create and wire circuit main compenets
     pRegCmp : ENTITY work.Reg GENERIC MAP(2*n+1) PORT MAP(pBs,startAndPause,clk,rst,pReg);
     mRegCmp : ENTITY work.Reg GENERIC MAP(n) PORT MAP(m,'1',start,rst,mReg);
     MuxCmp :  ENTITY work.BinaryMux GENERIC MAP(2*n+1) PORT MAP(pReg,pInit,counter(0),pMux);
     BSCmp : ENTITY work.ModifiedBoothStep GENERIC MAP(n) PORT MAP(pMux,mReg,pBs);
     StartCaptuerCmp : ENTITY work.TransitionDetector PORT MAP(start,clk,rst,restartDetection);
     CounterCmp : ENTITY work.ShiftReg GENERIC MAP(n/2) PORT MAP(counter,clk,startAndPause,counterRst);
-    TffCmp : ENTITY work.Reg GENERIC MAP(1) PORT MAP(D(0) => qBar,en => '1',clk => tffClk,rst => rst, Q(0) => working,Qbar(0) => qBar);
+    firtStartLachCmp : ENTITY work.Reg GENERIC MAP(1) PORT MAP(D(0) => '1',en => '1',clk => start,rst => rst, Q(0) => firstStart);
 
     -- output only valid if done is one
     f <= pBs(2*n DOWNTO 1);
