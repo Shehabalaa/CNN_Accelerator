9cb6cb62cf3b563cbe0852135a4d541a998b52b4
AmmarRabie <ammaralsayed55@gmail.com>
2019-04-11 15:24:52 +0200

    [CNN][DMA]: resolve some  issues

--- CNN/WriteDMA.vhd
+++ CNN/WriteDMA.vhd
@@ -4,54 +4,60 @@
 --when receiving        
 library IEEE;
 use ieee.std_logic_1164.all;
-    use ieee.numeric_std.all;
-    use work.constants.all;
-    Entity WriteDMA is
-       GENERIC(
-        addressSize: INTEGER:=16;
-        internalBusSize: INTEGER:=16
-       );
-        port(
-            clk:in std_logic;
-            -- Write logic interface
-            writeBaseAddress:IN std_logic_vector(addressSize-1 downto 0) ;
-            writeStep:IN std_logic_vector(maxImageSize-1 downto 0);
-            writeToRam:IN std_logic;
-            counter:In std_logic_vector(maxImageSize-1 downto 0) ;
-            initCounter,initAddress:IN std_logic;
-            internalBus:INOUT STD_LOGIC_VECTOR(internalBusSize-1 DOWNTO 0);
-           
-            --ram interface
-            ramWrite:out std_logic;
-            writeComplete:out std_logic;
-            ramDataInBus:out STD_LOGIC_VECTOR(weightsBusSize-1 DOWNTO 0);
-            ramWriteAddress:out std_logic_vector(addressSize-1 downto 0) ;
-            MFC:in std_logic
-        );
-        end WriteDMA;
-    architecture WriteDMAArch of WriteDMA is
-        signal toBeAdded :std_logic_vector(15 downto 0) ;  
-        signal currentCount:std_logic_vector(maxImageSize-1 downto 0) ;
-        constant zeros:std_logic_vector(maxImageSize-1 downto 0) :=(others=>'0');  
-        signal enableCounter:std_logic; 
+use ieee.numeric_std.all;
+use work.constants.all;
+
+Entity WriteDMA is
+    GENERIC(
+    addressSize: INTEGER:=16;
+    internalBusSize: INTEGER:=16
+    );
+    port(
+        clk:in std_logic;
+        -- Write logic interface
+        writeBaseAddress:IN std_logic_vector(addressSize-1 downto 0) ;
+        writeStep:IN std_logic_vector(addressSize-1 downto 0);
+        writeToRam:IN std_logic;
+        counter:In std_logic_vector(maxImageSize-1 downto 0) ;
+        initCounter,initAddress:IN std_logic;
+        
+        internalBus:INOUT STD_LOGIC_VECTOR(internalBusSize-1 DOWNTO 0);
+        --ram interface
+        ramWrite:out std_logic;
+        ramDataOutBus:out STD_LOGIC_VECTOR(weightsBusSize-1 DOWNTO 0);
+        ramWriteAddress:out std_logic_vector(addressSize-1 downto 0) ;
+        MFC:in std_logic;
+
+        writeComplete:out std_logic;
+        writeCompleteOne:out std_logic
+    );
+END WriteDMA;
+
+architecture WriteDMAArch of WriteDMA is
+    signal toBeAdded :std_logic_vector(addressSize-1 downto 0) ;  
+    signal currentCount:std_logic_vector(maxImageSize-1 downto 0) ;
+    constant zeros:std_logic_vector(maxImageSize-1 downto 0) :=(others => '0');
+    constant ones:std_logic_vector(maxImageSize-1 downto 0) :=(0 => '1', others => '0');
+    signal enableCounter:std_logic; 
+    begin
+        writeStepRegister:Entity work.Reg Generic Map(addressSize) PORT MAP(writeStep,'1',initCounter,'0',tobeAdded);
+        writeAddressRegister:Entity work.MultiStepCounter  Generic map(addressSize) port map(writeBaseAddress,tobeAdded,'0',clk,initAddress,MFC,ramWriteAddress);
+        writecounter:Entity work.DownCounter Generic map(maxImageSize) port map(counter,enableCounter,clk,initCounter,currentCount); 
+        
+        enableCounter <= MFC or initCounter;
+        writeCompleteOne <= MFC;
+        process(clk, MFC,writeToRam,currentCount,internalBus,initCounter, initAddress)
         begin
-            writeStepRegister:Entity work.Reg Generic Map(16) PORT MAP(writeStep,'1',initCounter,'0',tobeAdded);
-            writeAddressRegister:Entity work.MultiStepCounter  Generic map(addressSize) port map(writeBaseAddress,tobeAdded,'0',clk,initAddress,MFC,ramWriteAddress);
-            writecounter:Entity work.DownCounter Generic map(maxImageSize) port map(counter,enableCounter,clk,initCounter,currentCount); 
-           
-            process(MFC,writeToRam,currentCount,internalBus)
-            begin
-                if currentCount=zeros and  MFC='1' then
-                    writeComplete<='1';
-                else 
-                    writeComplete<='0';
-            end if;        
-            if writeToRam='1' then
-                ramWrite<='1';
-                ramDataInBus<=internalBus;
+            if MFC='1' AND ( (clk = '0' AND currentCount = ones) OR (clk = '1' AND currentCount = zeros))  THEN
+                writeComplete <= '1';
             else 
-                ramWrite<='0';    
-            end if;
-            enableCounter<=MFC or init;
-            end process;
-            end architecture;               
+                writeComplete <= '0';
+        end if;
+        if writeToRam='1' then
+            ramWrite<='1';
+            ramDataOutBus<=internalBus;
+        else
+            ramWrite<='0';
+        end if;
+    end process;
+end architecture;
