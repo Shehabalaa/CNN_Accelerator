9a244b0377b7323828efa0768928fe6589e55c6a
RamySaied1 <ramy.m.saied@gmail.com>
2019-04-13 20:47:45 +0200

    [FC][ngetmax] some modification in max (tony modification in my laptop )

--- FC/ngetMax.vhd
+++ FC/ngetMax.vhd
@@ -12,7 +12,7 @@ ENTITY ngetMax IS
      PORT( 
             inputArray : IN genericArrayofVector16bit(9 downto 0);
             en, clk, rst: in STD_LOGIC;
-            outLabel : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+            outLabel : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
             done: OUT STD_LOGIC
           );
 END ngetMax;
@@ -23,24 +23,27 @@ END ngetMax;
 
 ARCHITECTURE GetMaxArch OF ngetMax IS
 
-Component Reg IS
+Component RegTony IS
 	Generic(wordSize:integer :=32);
 	PORT(
 			D: in STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
+			load: in STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
 			en, clk, rst: in STD_LOGIC;
-			Q: out STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0)
+            Q: out STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
+            Qbar: out STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0)
 		);
 END Component;
-Component Counter IS
+Component CounterUpDown IS
 
-    GENERIC (n: integer := 4);
+    GENERIC (n: integer :=4);
 
-    PORT(
-        load: in STD_LOGIC_VECTOR(n-1 downto 0);
-        reset, clk, isLoad: IN STD_LOGIC;
-        count: out STD_LOGIC_VECTOR(n-1 downto 0)
-    );
+        PORT(
+            load,resetValue: in std_logic_vector(n-1 downto 0);
+            clk, en, rst, isLoad,upOrDown: in std_logic;
+            count: out std_logic_vector(n-1 downto 0)
+        );
 
+    
 END Component;
 Component Comparator IS
         GENERIC (wordSize : integer := 32);
@@ -55,30 +58,31 @@ END Component;
 
 Signal regMaxout : STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
 Signal regMaxin : STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
-Signal indexMaxin : STD_LOGIC_VECTOR(4 DOWNTO 0);
-Signal indexMaxout : STD_LOGIC_VECTOR(4 DOWNTO 0);
-Signal ComparatorG : STD_LOGIC;
+Signal indexMaxin : STD_LOGIC_VECTOR(3 DOWNTO 0);
+Signal indexMaxout : STD_LOGIC_VECTOR(3 DOWNTO 0);
+Signal ComparatorG : STD_LOGIC; 
 Signal ComparatorEqual : STD_LOGIC;
 Signal Count : STD_LOGIC_VECTOR (3 DOWNTO 0);
-signal InputBcomparator : STD_LOGIC_VECTOR(wordSize-1 downto 0);
-signal enableReg, enableModule : STD_LOGIC;
-signal counterEnable, loadCounter : STD_LOGIC;
-signal dCount: STD_LOGIC_VECTOR (3 DOWNTO 0);
+Signal InputBcomparator : STD_LOGIC_VECTOR(wordSize-1 downto 0);
+Signal enableReg : STD_LOGIC;
+Signal load: STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0):="1000000000000000";
+--Signal counterEnable, loadCounter : STD_LOGIC;
+  
 BEGIN
-    enableModule <= '1' when (en = '1' and  (unsigned(dCount) <= "1001")) else '0';
-    done <= '1' when Count = "01010" else '0';
-    dCount <= Count when (enableModule ='1' and  (unsigned(Count) <= "1001" and unsigned(Count) >= "0000")) else "0000";
-    regMaxValue : Reg generic map(wordSize) port map(regMaxin,enableModule,clk,rst,regMaxout);
-    regMaxIndex : Reg generic map(5) port map(indexMaxin,enableModule,clk,rst,indexMaxout);
-    InputBcomparator <= inputArray(to_integer(unsigned(dCount)));
-    CounterLabel : Counter generic map(5) port map("00000",rst,clk,'0',Count);
-    Comoarator: Comparator generic map(wordSize) port map(regMaxout,InputBcomparator,ComparatorG,ComparatorEqual);
-    
-    indexMaxin <= dCount when ComparatorG = '1' else indexMaxout;   
-    regMaxin <= ("1000000000000000" ) when dCount = "00000" 
-		else InputBcomparator when ComparatorG = '1' 
-		else regMaxout;
 
     
+    regMaxValue : RegTony generic map(wordSize) port map(regMaxin,load,en,clk,rst,regMaxout);
+    regMaxIndex : RegTony generic map(4) port map(indexMaxin,"0000",en,clk,rst,indexMaxout);
+    CounterLabel : CounterUpDown generic map(4) port map((others =>'0'),(others =>'0'),clk,en,rst,'0','0',Count);
+    ComparatorLabel: Comparator generic map(wordSize) port map(regMaxout,InputBcomparator,ComparatorG,ComparatorEqual);
+    
+    InputBcomparator <= inputArray(to_integer(unsigned(Count))) when Count <="1001" and rst ='0';
+    done <= '1' when Count = "1001" else '0';
+    indexMaxin <= Count when ComparatorG = '1' else indexMaxout;
+    
+    regMaxin <= ("1000000000000000" ) when rst='1'
+		else InputBcomparator when ComparatorG = '1' 
+		else regMaxout;
+   
     outLabel <= indexMaxout; 
 END GetMaxArch;
